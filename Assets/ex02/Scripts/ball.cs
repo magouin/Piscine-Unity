ba6499b7f37966f3c72e390c8b03264df8783b89
Maxime GOUIN <magouin@e2r1p10.42.fr>
2018-10-01 22:47:15 +0200

    finish

---
+++ Assets/ex02/Scripts/ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+	public float power;
+	public club g;
+	private float sens;
+	// Use this for initialization
+	void Start () {
+		power = 0f;
+		sens = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y > 5f)
+			sens = -1f;
+		if (transform.position.y > -5f)
+			sens = 1f;
+        transform.Translate(new Vector3(0, sens, 0) * Time.deltaTime * power);
+        power /= 1.05f * Time.deltaTime;
+        g.y = transform.position.y - 0.2f;
+	}
+}
---
+++ Assets/ex02/Scripts/club.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class club : MonoBehaviour {
+
+	public float y;
+	private float power;
+	public ball b;
+	// Use this for initialization
+	void Start () {
+		y = -3.73f;
+		power = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey("space"))
+        {
+            transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 0.2f);
+        	power += Time.deltaTime;
+        }
+        else
+        {
+        	b.power = power * 10f;
+        	transform.position = new Vector3(-0.2f, y, 0);
+			power = 0f;
+        }
+	}
+}
