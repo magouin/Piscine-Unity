1c7c78935679449215f3415554bc14a641a335c2
Maxime GOUIN <magouin@e2r3p15.42.fr>
2018-10-01 17:15:09 +0200

    Unity first commit

---
+++ Assets/ex00/Scripts/baloon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baloon : MonoBehaviour {
+
+	// Use this for initialization
+	private float size;
+	private	float time;
+	private int nb;
+
+	void Start () {
+		nb = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		size = Time.deltaTime * -0.2f;
+		time += Time.deltaTime;
+		if (Input.GetKeyDown(KeyCode.Space) && nb < 10)
+        {
+            size += 0.1f;
+			print(nb);
+			nb++;
+        }
+        if (transform.localScale.x > 3f || transform.localScale.x <= 0f)
+		{
+        	Destroy(gameObject);
+			print("Balloon life time: " + (int)time + "s");
+		}
+		if (((int)time % 3 == 0))
+		{
+			nb = 0;
+		}
+		transform.localScale += new Vector3(size, size, 0);
+	}
+}
