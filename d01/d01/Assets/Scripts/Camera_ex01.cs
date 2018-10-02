7d9535888a6c86f412e43296d0a16bb1132ae615
Maxime GOUIN <magouin@e1r4p13.42.fr>
2018-10-02 20:14:58 +0200

    day01

---
+++ d01/d01/Assets/Scripts/Camera_ex01.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Camera_ex01 : MonoBehaviour {
+
+	private int input;
+	private Vector3 default_position_claire;
+	private Vector3 default_position_john;
+	private Vector3 default_position_thomas;
+
+	public	playerScript_ex01 claire;
+	public	playerScript_ex01 john;
+	public	playerScript_ex01 thomas;
+
+	// Use this for initialization
+	void Start () {
+		input = 1;
+		default_position_claire = new Vector3(-4, 0.1f, 0);
+		default_position_john = new Vector3(-2, 0.25f, 0);
+		default_position_thomas = new Vector3(-1, 0.25f, 0);
+		claire.transform.position = default_position_claire;	
+		john.transform.position = default_position_john;	
+		thomas.transform.position = default_position_thomas;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("1"))
+			input = 1;
+		if (Input.GetKeyDown("2"))
+			input = 2;
+		if (Input.GetKeyDown("3"))
+			input = 3;
+		if (input == 1)
+			transform.position = new Vector3(claire.transform.position.x, claire.transform.position.y, transform.position.z);
+		if (input == 2)
+			transform.position = new Vector3(john.transform.position.x, john.transform.position.y, transform.position.z);
+		if (input == 3)
+			transform.position = new Vector3(thomas.transform.position.x, thomas.transform.position.y, transform.position.z);
+		if (Input.GetKeyDown("r"))
+		{
+			claire.transform.position = default_position_claire;	
+			john.transform.position = default_position_john;	
+			thomas.transform.position = default_position_thomas;	
+		}
+		if (claire.finish == 1 && john.finish == 1 && thomas.finish == 1)
+				SceneManager.LoadScene("ex02", LoadSceneMode.Single);
+	}
+}
---
+++ d01/d01/Assets/Scripts/playerScript_ex00.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript_ex00 : MonoBehaviour {
+
+	public int go_id;
+	public float masse;
+	public float size_x;
+	public float size_y;
+	public Rigidbody2D rb;
+	public int finish;
+
+	private int input;
+	private int can_jump;
+
+	void Start () {
+		input = 1;
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.constraints |= RigidbodyConstraints2D.FreezePositionX;
+		if (Input.GetKeyDown("1") || Input.GetKeyDown("2") || Input.GetKeyDown("3"))
+		{
+			rb.constraints |= RigidbodyConstraints2D.FreezePositionX;
+			if (Input.GetKeyDown("1"))
+				input = 1;
+			if (Input.GetKeyDown("2"))
+				input = 2;
+			if (Input.GetKeyDown("3"))
+				input = 3;
+		}
+		if (go_id == input)
+		{
+			rb.constraints &= ~RigidbodyConstraints2D.FreezePositionX;
+			if (Input.GetKey("space") && can_jump == 1)
+			{
+				rb.AddForce(new Vector2(0, 1) * 100 * (4 - masse));
+				can_jump = 0;
+			}
+			if (Input.GetKey(KeyCode.LeftArrow))
+				rb.AddForce(new Vector2(-1, 0) * 10);
+			if (Input.GetKey(KeyCode.RightArrow))
+				rb.AddForce(new Vector2(1, 0) * 10);
+		}
+	}
+
+	private void OnCollisionStay2D(Collision2D coll)
+    {
+		foreach (ContactPoint2D missileHit in coll.contacts)
+		{
+			if (missileHit.normal == new Vector2(0, 1))
+			{
+				can_jump = 1;
+				return ;
+			}
+		}
+		can_jump = 0;
+	}
+}
