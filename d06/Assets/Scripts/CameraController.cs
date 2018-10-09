9258a38cd040f3083f20e423e3f9af312d4a9e97
Maxime GOUIN <magouin@e3r12p19.42.fr>
2018-10-09 17:30:53 +0200

    Git c rigolo

---
+++ d06/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public float horizontalSpeed = 2.0f;
+    public float verticalSpeed = 2.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+        // Get the mouse delta. This is not in the range -1...1
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(v, h, 0);
+    }
+}
